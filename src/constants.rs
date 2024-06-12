b3beef1d82a99bf80eacb91a4b7f2c5b84631edc
end91x <end91@proton.me>
2024-06-12 10:35:10 +0200

    Changed max speed of ball

--- src/constants.rs
+++ src/constants.rs
@@ -9,7 +9,7 @@ pub const PADDLE_BOUNCE_ANGLE_FACTOR: f32 = 0.3;
 pub const BALL_RADIUS: f32 = 15f32;
 pub const BALL_SPEED: f32 = 6f32;
 pub const BALL_SPEED_MULTIPLIER: f32 = 1.05;
-pub const MAX_BALL_SPEED: f32 = 2f32;
+pub const MAX_BALL_SPEED: f32 = 3f32;
 
 pub const BG_COLOR: Color = Color::new(0.19, 0.69, 0.30, 1.0);
 pub const PADDLE_COLOR: Color = WHITE;
